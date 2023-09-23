6347857e873a8428b9e03c3568c15cee2d02a86a
Glauber Marques <glauberfmarques@gmail.com>
2023-09-22 21:05:53 -0600

    Exercise number e calculate revenue and average following established criteria

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,15 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+# Calculate and output the total revenue for all stores
+total_revenue = Store.sum(:annual_revenue)
+puts "Total Company Revenue: $#{total_revenue}"
+
+# Calculate and output the average annual revenue for all stores
+average_revenue = Store.average(:annual_revenue)
+puts "Average Annual Revenue for All Stores: $#{average_revenue}"
+
+# Count and output the number of stores generating $1M or more in annual sales
+high_revenue_stores = Store.where('annual_revenue >= ?', 1000000).count
+puts "Number of Stores Generating $1M or More in Annual Sales: #{high_revenue_stores}"
+
