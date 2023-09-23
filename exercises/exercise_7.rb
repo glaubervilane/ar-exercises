ca4303893b93d36b2ad6223bef58d67f253b3f51
Glauber Marques <glauberfmarques@gmail.com>
2023-09-23 10:31:37 -0600

    Add exercise 7, validate user inputs

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,33 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+# Ask the user for a store name
+print "Enter a store name: "
+store_name = gets.chomp
+
+# Ask the user for annual revenue, mens_apparel, and womens_apparel
+print "Enter annual revenue: "
+annual_revenue = gets.chomp.to_i
+
+print "Carries men's apparel (true/false): "
+mens_apparel = gets.chomp.downcase == 'true'
+
+print "Carries women's apparel (true/false): "
+womens_apparel = gets.chomp.downcase == 'true'
+
+# Create a new store with all attributes
+new_store = Store.new(
+  name: store_name,
+  annual_revenue: annual_revenue,
+  mens_apparel: mens_apparel,
+  womens_apparel: womens_apparel
+)
+
+# Check if the store is valid and attempt to save it
+if new_store.valid?
+  new_store.save
+  puts "Store '#{store_name}' was successfully created."
+else
+  puts "Store validation failed. Errors:"
+  new_store.errors.full_messages.each { |msg| puts msg }
+end
