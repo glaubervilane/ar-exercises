176fa57d05bd003780bf21713aa0f0dc69cefe9a
Glauber Marques <glauberfmarques@gmail.com>
2023-09-22 20:27:57 -0600

    Exercise number 2 update the first store name

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,11 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+@store1.update(name: 'Super Store')
+@store1.save
+
+
+updated_store_name = Store.find(1).name
+puts "Updated store name: #{updated_store_name}"
